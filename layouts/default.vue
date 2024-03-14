637f41a77d98fb392dbcbc8a783181b98d941bd5
Nick van Raaij <45543766+venraij@users.noreply.github.com>
2024-03-14 10:36:18 +0100

    initial dashboard layout (#1) (#2)
    * initial dashboard layout
    
    * setup stuff for web3
    
    * responsive menu
    
    * metamask connect
    
    * publish to cloudflare pages
    
    * setup deployments
    
    * deployment stuff
    
    * remove workflow
    
    * trigger dep
    
    * remove space

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-layout class="rounded rounded-md">
+    <AppHeader />
+    <AppSidebar />
+
+    <v-main
+      class="d-flex align-center justify-center"
+      style="min-height: 300px;"
+    >
+      <slot />
+    </v-main>
+  </v-layout>
+</template>
+
+<script setup lang="ts">
+import { useWeb3Store } from '#imports'; 
+
+const web3 = useWeb3Store()
+
+onMounted(() => {
+  web3.connect();
+})
+
+</script>
