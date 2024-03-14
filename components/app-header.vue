9e4107284576452452c6a09625b295506dd06db3
Nick van Raaij <nick.van.raaij@topicus.nl>
2024-03-14 17:36:58 +0100

    wallet connect instead of web3

--- components/app-header.vue
+++ components/app-header.vue
@@ -1,19 +1,31 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <v-app-bar title="Muneys App">
+    <v-avatar 
+      class="wallet-avatar"
+      icon="$vuetify" 
+      color="green"
+      @click="web3.connect()"
+    />
     <v-app-bar-nav-icon
       class="d-flex d-md-none" 
       @click="app.toggleDrawer()"
     />
-    <template #prepend />
   </v-app-bar>
 </template>
 
 <script setup lang="ts">
 import { useAppStore } from '~/stores/app';
+import { useWeb3Store } from '#imports'; 
 
 const app = useAppStore()
-
-
+const web3 = useWeb3Store()
 
 </script>
+
+<style lang="scss">
+.wallet-avatar {
+  cursor: pointer;
+}
+
+</style>
