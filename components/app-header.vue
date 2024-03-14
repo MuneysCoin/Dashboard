a5d8918e3db98c2aca0640408c21188947746ddc
Nick van Raaij <dagelijksnick@gmail.com>
2024-03-14 22:57:01 +0100

    test

--- components/app-header.vue
+++ components/app-header.vue
@@ -5,7 +5,7 @@
       class="wallet-avatar"
       icon="$vuetify" 
       color="green"
-      @click="web3.connect()"
+      @click="modal.open()"
     />
     <v-app-bar-nav-icon
       class="d-flex d-md-none" 
@@ -16,10 +16,44 @@
 
 <script setup lang="ts">
 import { useAppStore } from '~/stores/app';
-import { useWeb3Store } from '#imports'; 
+import { useWeb3Modal } from '@web3modal/wagmi/vue'
 
 const app = useAppStore()
-const web3 = useWeb3Store()
+
+
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi'
+
+import { mainnet, arbitrum } from 'viem/chains'
+import { reconnect } from '@wagmi/core'
+
+// 1. Define constants
+const projectId = 'b7234ee880f91dcffd7229b192a94c55'
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: 'Web3Modal',
+  description: 'Web3Modal Example',
+  url: 'https://app.muneys.com', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+const chains = [mainnet, arbitrum]
+const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata
+})
+
+reconnect(config)
+// 3. Create modal
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true // Optional - false as default
+})
+
+const modal = useWeb3Modal()
 
 </script>
 
