cad0c8d3ace546d8d6e398abad971bec06c8605e
Nick van Raaij <dagelijksnick@gmail.com>
2024-03-15 10:01:49 +0100

    add testnet support

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@
 
 <script setup lang="ts">
 import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi'
-import { bsc } from 'viem/chains'
+import { bsc, bscTestnet } from 'viem/chains'
 import { reconnect } from '@wagmi/core'
 
  // 1. Define constants
@@ -24,10 +24,14 @@ const tokens: any = {
   1: {
     address: "0x74c57783b980a44efa4741ba863a71aded83f71c",
     image: "https://muneys.com/wp-content/uploads/2024/03/cropped-cropped-OIG2-photoaidcom-cropped-150x150.png"
+  },
+  2: {
+    address: "0xe93365962fF59a26892CE3521eE3823Fa707Bc9C",
+    image: "https://muneys.com/wp-content/uploads/2024/03/cropped-cropped-OIG2-photoaidcom-cropped-150x150.png"
   }
 }
 
-const chains: any = [bsc]
+const chains: any = [bsc, bscTestnet]
 const config = defaultWagmiConfig({
   chains,
   projectId,
