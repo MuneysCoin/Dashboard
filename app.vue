f4948b4997de283ca9ace0ab6358f9e6119d703a
Nick van Raaij <dagelijksnick@gmail.com>
2024-03-15 07:36:32 +0100

    fixes

--- app.vue
+++ app.vue
@@ -3,3 +3,49 @@
     <NuxtPage /> 
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi'
+
+import { bsc } from 'viem/chains'
+import { reconnect } from '@wagmi/core'
+
+// 1. Define constants
+const projectId = 'b7234ee880f91dcffd7229b192a94c55'
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: 'Web3Modal',
+  description: 'Connect your wallet',
+  url: 'https://app.muneys.com', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+const tokens: any = {
+  1: {
+    address: "0x74c57783b980a44efa4741ba863a71aded83f71c",
+    image: "https://muneys.com/wp-content/uploads/2024/03/cropped-cropped-OIG2-photoaidcom-cropped-150x150.png"
+  }
+}
+
+const chains: any = [bsc]
+const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata
+})
+
+if (process.client) {
+  reconnect(config)
+
+  // 3. Create modal
+  createWeb3Modal({
+    wagmiConfig: config,
+    projectId,
+    enableAnalytics: true, // Optional - defaults to your Cloud configuration
+    enableOnramp: true, // Optional - false as default,
+    themeMode: 'dark',
+    tokens
+  })
+}
+</script>
--- components/app-header.vue
+++ components/app-header.vue
@@ -16,55 +16,11 @@
 
 <script setup lang="ts">
 import { useAppStore } from '../stores/app';
-import { useWeb3Modal } from '@web3modal/wagmi/vue'
-import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi'
-
-import { bsc } from 'viem/chains'
-import { reconnect } from '@wagmi/core'
 
 const app = useAppStore()
+import { useWeb3Modal } from '@web3modal/wagmi/vue'
 
-// 1. Define constants
-const projectId = 'b7234ee880f91dcffd7229b192a94c55'
-
-// 2. Create wagmiConfig
-const metadata = {
-  name: 'Web3Modal',
-  description: 'Connect your wallet',
-  url: 'https://app.muneys.com', // origin must match your domain & subdomain
-  icons: ['https://avatars.githubusercontent.com/u/37784886']
-}
-
-const tokens = {
-  1: {
-    address: "0x74c57783b980a44efa4741ba863a71aded83f71c",
-    image: "https://muneys.com/wp-content/uploads/2024/03/cropped-cropped-OIG2-photoaidcom-cropped-150x150.png"
-  }
-}
-
-const chains = [bsc]
-const config = defaultWagmiConfig({
-  chains,
-  projectId,
-  metadata,
-  tokens
-})
-
-if (process.client) {
-  reconnect(config)
-}
-
-// 3. Create modal
-createWeb3Modal({
-  wagmiConfig: config,
-  projectId,
-  enableAnalytics: true, // Optional - defaults to your Cloud configuration
-  enableOnramp: true, // Optional - false as default,
-  themeMode: 'dark'
-})
-
-// const modal = useWeb3Modal()
-
+const modal = useWeb3Modal()
 
 </script>
 
